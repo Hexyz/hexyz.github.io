03978a712eef6ca17bb57ce0fa5b0ad63d99e3a1
H Ξ Ⅹ〒Ƶ <Hexyz@Self.local>
2020-12-23 20:03:22 +0800

    Site updated: 2020-12-23 20:03:22

---
+++ documents/install.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+
+sudo apt-get install libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-dev g++-multilib -y &&
+sudo apt-get install -y git flex bison gperf build-essential libncurses5-dev:i386 -y &&
+sudo apt-get install tofrodos python-markdown libxml2-utils xsltproc zlib1g-dev:i386 -y &&
+sudo apt-get install dpkg-dev libsdl1.2-dev libesd0-dev -y &&
+sudo apt-get install git-core gnupg flex bison gperf build-essential -y &&
+sudo apt-get install zip curl zlib1g-dev gcc-multilib g++-multilib -y &&
+sudo apt-get install libc6-dev-i386 -y &&
+sudo apt-get install lib32ncurses5-dev x11proto-core-dev libx11-dev -y &&
+sudo apt-get install libgl1-mesa-dev libxml2-utils xsltproc unzip m4 -y &&
+sudo apt-get install lib32z-dev ccache -y
